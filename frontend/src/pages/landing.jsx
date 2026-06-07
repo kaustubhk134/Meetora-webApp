2ae86272f0a6d68befe03eb4f0772c287518ed3d
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2026-06-08 04:23:48 +0530

    completed the ui of landing page, added the phone images and created the path for home

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css"
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
     return (
@@ -16,6 +17,19 @@ export default function LandingPage() {
                     </div>
                 </div>
             </nav>
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect </span>with your loved Ones</h1>
+                    <p>Cover a distance by Meetora</p>
+                    <div role="button">
+                        <Link to={"/home"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+                    <img src='/mobile.png'></img>
+                </div>
+            </div>
         </div>
     )
 }
