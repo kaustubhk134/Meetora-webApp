336be441d62844b8d70d019d4aafc61ea7e57135
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2026-06-08 03:53:53 +0530

    setup the skleton header of the head page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -4,6 +4,18 @@ import "../App.css"
 export default function LandingPage() {
     return (
         <div class='landingPageContainer'>
+            <nav>
+                <div className="navHeader">
+                    <h2>Meetora Video Call</h2>
+                </div>
+                <div className="navList">
+                    <p>Join as Guest</p>
+                    <p>Register</p>
+                    <div role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
